f4660df7e1a84c8cd453a4771da0f35e66ba82bc
= <=>
2021-02-07 19:05:37 +0500

    initial commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,16 @@ namespace _portfolio.Controllers
 {
     public class HomeController : Controller
     {
+        public ViewResult Portfolio()
+        {
+            return View();
+        }
+
+        public ViewResult AboutMe()
+        {
+            return View();
+        }
+
         public ViewResult Index()
         {
             return View();
