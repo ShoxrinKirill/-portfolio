46ff12ffb41cf5b7b1da57ab48b2ad7d0bfa4cb7
= <=>
2021-02-15 00:40:34 +0500

    Portfolio design changed, examples model added

---
+++ Data/Models/Examles.cs
@@ -0,0 +1,10 @@
+namespace _portfolio.Data.Models
+{
+    public class Examles
+    {
+        public string nameExample {set; get;}
+        public string img {set; get;}
+        public string gitUrl {set; get;}
+        public string description {set; get;}
+    }
+}
